7d9ffead361bf6bc0f5cb398d4eec898b1efc0db
Chen Canhao <chchen@thoughtworks.com>
2017-11-22 22:18:16 +0800

    update

--- src/app/footer-link/footer-link.component.ts
+++ src/app/footer-link/footer-link.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'footer-link',
+  selector: 'app-footer-link',
   templateUrl: './footer-link.component.html',
   styleUrls: ['./footer-link.component.scss']
 })
