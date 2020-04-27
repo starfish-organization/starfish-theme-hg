7e1f8278ae08aebff34c63f29ad9506c6ac75a4a
fwchen <me@chenfangwei.com>
2020-04-27 18:15:14 +0800

    update

--- src/app/core/nav-logo/nav-logo.component.ts
+++ src/app/core/nav-logo/nav-logo.component.ts
@@ -1,20 +1,8 @@
-import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-logo',
   templateUrl: './nav-logo.component.html',
   styleUrls: ['./nav-logo.component.scss']
 })
-export class NavLogoComponent implements OnInit, AfterContentInit {
-  contentInited = false;
-
-  constructor() {}
-
-  ngOnInit() {}
-
-  ngAfterContentInit() {
-    setTimeout(() => {
-      this.contentInited = true;
-    }, 300);
-  }
-}
+export class NavLogoComponent {}
